5b08c687d14b4587354017949459a738d9170d58
gabs1san <sanbiel398@gmail.com>
2026-05-27 18:19:06 -0300

    Add project files.

---
+++ LinqToApi/Program.cs
@@ -0,0 +1,17 @@
+﻿using (HttpClient client = new HttpClient())
+{
+    string response = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+    Console.WriteLine(response);
+}
+
+namespace LinqToApi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+ 
