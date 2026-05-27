991eeed0a91913eab4474e6d2a94e8027af9491f
gabs1san <sanbiel398@gmail.com>
2026-05-27 18:58:54 -0300

    Aprendendo a utilizar o JsonPropertyName

--- LinqToApi/Models/Music.cs
+++ LinqToApi/Models/Music.cs
@@ -1,8 +1,28 @@
-﻿
+﻿using System.Text.Json.Serialization;
 
 namespace LinqToApi.Models
 {
     internal class Music
     {
+        [("Song")]
+        public string Nome { get; set; }
+
+        [JsonPropertyName("artist")]
+        public string Artist { get; set; }
+
+        [JsonPropertyName("duration_ms")]
+        public int Duration { get; set; }
+
+        [JsonPropertyName("gendre")]
+        public string Gender { get; set; }
+    
+
+    public void PrintMusicInfo(Music music)
+        {
+            Console.WriteLine($"Nome: {music.Nome}");
+            Console.WriteLine($"Artist: {music.Artist}");
+            Console.WriteLine($"Duration: {music.Duration} ms");
+            Console.Write(music.Nome);
+        }
     }
 }
